f8c0c2d7cfec2ffb493328a4126ea1359fec1149
chandu <chandrasr.palle@gmail.com>
2018-05-17 09:37:44 +0200

    step1 added

--- week1/main.js
+++ week1/main.js
@@ -1,1 +1,14 @@
 console.log("Hello JS");
+
+// ------------------- Step : 1 ------------------------------
+let numbers = [1, 2, 3, 4];
+// let newNumbers = [];
+// for(let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 !== 0) {
+//         newNumbers[i] = numbers[i] * 2;
+//     }
+// }
+let newNumbers = numbers.filter(number => number%2 !== 0).map(number => number * 2);
+console.log("The doubled numbers are", newNumbers); // [2, 6]
+
+// -------------------Step : 2 ---------------------------------

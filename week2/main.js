f4d74cf9c4cf2f100bd88e9549c1cd3832509567
chandu <chandrasr.palle@gmail.com>
2018-05-25 12:35:28 +0200

    code refactored to simple functions

--- week2/main.js
+++ week2/main.js
@@ -1,32 +1,42 @@
-let stepOne = (function() {
-  let generateArray = function(arrayLength) {
-    let arr = [];
-    for (let i = 1; i <= arrayLength; i++) {
-      arr.push(i);
-    }
-    return arr;
-  };
+function generateArray(arrayLength) {
+  let arr = [];
+  for (let i = 1; i <= arrayLength; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
-  return {
-    divisibleFactory: function(lengthOfArr, dividedBy) {
-      return generateArray(lengthOfArr).filter(
-        number => number % dividedBy == 0
-      );
-    },
-    dividedNumberOfTimesArr: function(divisibleArrLength, lengthOfMainArr) {
-      return generateArray(divisibleArrLength).map(
-        number => this.divisibleFactory(lengthOfMainArr, number).length
-      );
-    }
-  };
-})();
+function divisibleFactory(dividedBy) {
+  let divisibleNumbersArray = numbers.filter(number => number % dividedBy == 0);
 
-console.log(
-  "Numbers divisible by 3 for the array length 1000 : ",
-  stepOne.divisibleFactory(1000, 3)
-);
+  function divisibilityChecker(n) {
+    return divisibleNumbersArray.includes(n)
+    ;
+  }
+  return divisibilityChecker;
+}
 
-console.log(
-  "Array divisible by numbers between 1-30 for the array length of 1000",
-  stepOne.dividedNumberOfTimesArr(30, 1000)
-);
+const numbers = generateArray(1000);
+const dividers = generateArray(30);
+
+const divisiblebByThreeFunction = divisibleFactory(3);
+console.log(divisiblebByThreeFunction(1));
+console.log(divisiblebByThreeFunction(9));
+console.log(divisiblebByThreeFunction(10));
+
+const divisiblebByFiveFunction = divisibleFactory(5);
+console.log(divisiblebByFiveFunction(1));
+console.log(divisiblebByFiveFunction(9));
+console.log(divisiblebByFiveFunction(10));
+
+const divisiblebByTenFunction = divisibleFactory(10);
+console.log(divisiblebByTenFunction(1));
+console.log(divisiblebByTenFunction(9));
+console.log(divisiblebByTenFunction(10));
+
+function dividedNumberOfTimesArr() {
+  return dividers.map(
+    divider => numbers.filter(number => number % divider == 0).length
+  );
+}
+console.log(dividedNumberOfTimesArr());

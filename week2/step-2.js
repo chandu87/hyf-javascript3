e01432c1b12ef2d459f4491f83a64c46b7d4f4e0
chandu <chandrasr.palle@gmail.com>
2018-05-27 09:44:44 +0200

    new promise method added

--- week2/step-2.js
+++ week2/step-2.js
@@ -8,9 +8,20 @@ const radioBtnVgood = document.querySelector("#radio-vgood");
 const radioBtnGood = document.querySelector("#radio-good");
 
 //Function call to Start the app (for accessdata)
-
 showMovieData();
 
+// Function for retrieving data
+function showMovieData() {
+    getAjaxData(
+      "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
+    )
+      .then(data => {
+        handleData(data);
+      })
+      .catch(error => {
+        message.innerHTML = error;
+      });
+  }
 // function for getting data with given URL
 function getAjaxData(url) {
   return new Promise((resolve, reject) => {
@@ -35,66 +46,73 @@ function getAjaxData(url) {
   });
 }
 
-// Function for retrieving data
-function showMovieData() {
-  getAjaxData(
-    "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
-  )
-    .then(data => {
-      handleData(data);
-    })
-    .catch(error => {
-      message.innerHTML = error;
-    });
-}
-
-//Function for handle data
+//Function for handle data and Listen for button click to filter movies
 function handleData(moviesData) {
   if (moviesData.length > 0) {
     message.innerHTML = "Total Number of Movies " + moviesData.length;
     moviesData.forEach(addTagForMovie); // Adding Tag for each movie based on rating
-
+    let avgRating;
     const reducer = (accumulator, currentValue) =>
       accumulator + currentValue.rating;
-    let searchFor, avgRating, searchMovieTag;
-    
+
     searchButton.addEventListener("click", function() {
-      ulList.innerHTML = "";
-      searchFor = searchKeyword.value;
-      searchKeyword.value = "";
-      searchMovieTag = "";
-      if (radioBtnExcellent.checked) {
-        searchMovieTag = "Excellent";
-      } else if (radioBtnVgood.checked) {
-        searchMovieTag = "Very Good";
-      } else if (radioBtnGood.checked) {
-        searchMovieTag = "Good";
-      }
-      const filteredMovies = moviesData.filter(movie => {
-        if (searchMovieTag === "") {
-          return movie.title.includes(searchFor);
-        } else {
-          return (
-            movie.title.includes(searchFor) && movie.tag === searchMovieTag
-          );
-        }
-      });
+      filterMovies(moviesData)
+      .then(movies => {
+        avgRating = Math.round(
+          // Calculating average rating
+          movies.reduce(reducer, 0) / movies.length
+        );
+        message.innerHTML =
+          "Average Rating of total " +
+          movies.length +
+          " Filtered movies is " +
+          avgRating;
 
-      avgRating = Math.round(  // Calculating average rating
-        filteredMovies.reduce(reducer, 0) / filteredMovies.length
-      );
-
-      message.innerHTML = "Average Rating of Filtered movies is " + avgRating;
-
-      filteredMovies.forEach(movie => {
-        const liItem = document.createElement("li");
-        ulList.appendChild(liItem);
-        liItem.innerHTML = movie.title;
+        movies.forEach(movie => {
+          const liItem = document.createElement("li");
+          ulList.appendChild(liItem);
+          liItem.innerHTML = movie.title;
+        });
+      })
+      .catch((error)=>{
+        message.innerHTML = error;
       });
     });
   }
 }
+//Function to filter movies then resolve the success and reject the failure case
+//Parameters moviesData, [] - array
+//return Promise
+function filterMovies(moviesData) {
+  return new Promise((resolve, reject) => {
+    let searchFor, searchMovieTag;
 
+    ulList.innerHTML = "";
+    searchFor = searchKeyword.value;
+    searchKeyword.value = "";
+    searchMovieTag = "";
+    if (radioBtnExcellent.checked) {
+      searchMovieTag = "Excellent";
+    } else if (radioBtnVgood.checked) {
+      searchMovieTag = "Very Good";
+    } else if (radioBtnGood.checked) {
+      searchMovieTag = "Good";
+    }
+    const filteredMovies = moviesData.filter(movie => {
+      if (searchMovieTag === "") {
+        return movie.title.includes(searchFor);
+      } else {
+        return movie.title.includes(searchFor) && movie.tag === searchMovieTag;
+      }
+    });
+
+    if (filteredMovies.length > 0) {
+      resolve(filteredMovies);
+    } else {
+      reject("No Results Found, Are you looking for something else");
+    }
+  });
+}
 //Function for adding Tags for each movie based on their Rating
 function addTagForMovie(movie) {
   let tagName = "";

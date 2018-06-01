76dd15ddea030f05532356575d25c72fb2b5a044
chandu <chandrasr.palle@gmail.com>
2018-06-01 11:37:14 +0200

    functionality added in prototypes

--- week3/main.js
+++ week3/main.js
@@ -1,16 +1,51 @@
-console.log('main.js');
-const initialStudents = [ 'niels', 'mads' ];
-const room = '3a';
+const initialStudents = ["niels", "mads"];
+const room = "3a";
 
 const students = StudentsFactory(initialStudents, room);
 // { add, delete ...... }
+function StudentsFactory(initialStudents, room) {
+  let totalStudents = initialStudents,
+    classRoom = room;
+  const addStudent = student => {
+    totalStudents.push(student);
+    console.log(`Added student ${student} to Class room : ${totalStudents}`);
+  };
+  const deleteStudent = student => {
+    const index = totalStudents.indexOf(student);
+    if (index != -1) {
+      totalStudents.splice(index, 1);
+      console.log(`Deleted student ${student} from Class : ${totalStudents}`);
+    } else {
+      console.log(`Student ${student} not found in the class room`);
+    }
+  };
+  const printAll = () => {
+    console.log(
+      `Class room : ${classRoom} and Total students array is :  [${totalStudents}]`
+    );
+  };
+  const deleteAll = () => {
+    totalStudents = [];
+  };
+  const changeRoom = newRoom => {
+    classRoom = newRoom;
+    console.log(classRoom);
+  };
+  return {
+    add: addStudent,
+    delete: deleteStudent,
+    printAll: printAll,
+    deleteAll: deleteAll,
+    changeRoom: changeRoom
+  };
+}
 
-students.add('younes');
+students.add("younes");
 // add name to the studens array // [ 'niels', 'mads', 'younes' ]
-students.add('johnny');
+students.add("johnny");
 // same // [ 'niels', 'mads', 'younes', 'johnny' ]
 
-students.delete('younes');
+students.delete("younes");
 // delete // [ 'niels', 'mads', 'johnny' ]
 
 students.printAll();
@@ -24,4 +59,4 @@ students.printAll();
 // 3a
 //  print all the names ''
 
-students.changeRoom('3b');
+students.changeRoom("3b");
